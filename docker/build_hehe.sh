4195bc47a3bd1150021b6fb67a322c7836d96be0
pattylo <19451945as@gmail.com>
2023-10-27 12:28:24 +0800

    add docker

---
+++ docker/build_hehe.sh
@@ -0,0 +1,25 @@
+
+
+final_name=$1
+distro=$1
+echo "BUILD AIRO DOCKER IMAGE OF $distro"
+
+echo "GOT GPU? y/n:"
+read got_gpu
+
+if [ "$got_gpu" == "y" ] || [ "$got_gpu" == "Y" ]; then
+    distro="${distro}-gpu"
+    final_name="${final_name}-gpu"
+elif [ "$got_gpu" == "n" ] || [ "$got_gpu" == "N" ]; then
+    distro="${distro}-nogpu"
+    final_name="${final_name}-nogpu"
+else
+    echo "PLEASE CHECK YOUR INPUT!"
+    exit 1
+fi
+
+echo "BUILDING $final_name DOCKER IMAGE."
+
+docker build \
+    -f dkerfiles/Dockerfile.$distro \
+    -t airo_noetic_hehe:$final_name .
